908c011dd1a618f308ff15f7a662a779fe421265
spencerwjensen96 <spencerwjensen96@gmail.com>
2021-03-03 23:56:41 -0700

    stuff

--- demoproject/main.js
+++ demoproject/main.js
@@ -1,1 +1,7 @@
-console.log('coding is fun');
+function add(n1, n2) {
+    return n1 + n2;
+}
+var number1 = 4;
+var number2 = 5;
+var result = add(number1, number2);
+console.log(result);
--- demoproject/main.ts
+++ demoproject/main.ts
@@ -1,1 +1,9 @@
-console.log('coding is fun');
+function add(n1 : number, n2 : number){
+    return n1+n2;
+}
+
+const number1 = 4;
+const number2 = 5;
+
+const result = add(number1, number2);
+console.log(result);
